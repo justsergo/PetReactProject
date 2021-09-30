579831763e955bcb61ff166ea8bc62e8fd2e119f
sergo <sergeydanilov2019@gmail.com>
2021-09-30 16:16:50 +0300

    make all on classes

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -7,6 +7,13 @@ import EmployeesAddForm from '../employees-add-form/employees-add-form';
 import './app.css';
 
 function App() {
+
+  const data = [
+    {name: 'John', salary: '1000', increase: false},
+    {name: 'Smith', salary: '500', increase: true},
+    {name: 'Sergr', salary: '400', increase: false},
+  ];
+
   return (
     <div className="app">
         <AppInfo />
@@ -16,7 +23,7 @@ function App() {
             <AppFilter/>
         </div>
         
-        <EmployeesList/>
+        <EmployeesList data={data}/>
         <EmployeesAddForm/>
     </div>
   );
